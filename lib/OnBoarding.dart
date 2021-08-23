3d42fc30664f8a04f5bcf140a2ed29af427b2674
Bob <bob.yuan@yahooo.com>
2021-08-22 20:50:21 -0700

    Added onboarding page

--- lib/OnBoarding.dart
+++ lib/OnBoarding.dart
@@ -1,9 +1,14 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class OnBoarding extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'avenir'
+      ),
       home: onboarding(),
     );
   }
@@ -14,9 +19,65 @@ class onboarding extends StatefulWidget {
 }
 
 class _onboardingState extends State<onboarding> {
+  int currentPage = 0;
+  PageController _pageController = new PageController(
+    initialPage: 0,
+    keepPage: true,
+  );
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height*0.6,
+                child: PageView(
+                  controller: _pageController,
+                  children: [
+                    onBoardPage("onboard1", "Welcome to Aking"),
+                    onBoardPage("onboard1", "Work Happens"),
+                    onBoardPage("onboard1", "Task and Assignemtn"),
+                  ],
+                  onPageChanged: (value) => {setCurrentPage(value)},
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Column onBoardPage(String img, String title)
+  {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 20,
+        ),
+        Container(
+          height: 200,
+          width: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.all(50),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('asset/image/$img.png')
+            )
+          ),
+        ),
+      ],
+    );
+  }
+
+  setCurrentPage(int value){
+    currentPage = value;
+    setState(() {
+
+    });
   }
 }
 
